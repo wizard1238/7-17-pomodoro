3a7aaf3ebdeb60021bd31ab8384e2ea6ccb27ad9
Adelina C <73216866+adelly13@users.noreply.github.com>
2021-07-17 13:59:07 -0700

    Created clock face and made hand move

--- 7-17-pomodoro/ContentView.swift
+++ 7-17-pomodoro/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var myVar: String = "my text"
+    @State var receiver = Timer.publish(every: 1, on: .current, in: .default).autoconnect()
+    @State var second: Int = 0
+    var circleDiameter: CGFloat = UIScreen.main.bounds.width * 0.9
+    
     var body: some View {
-        Text(myVar)
-            .padding()
-        
+        ZStack {
+            Circle()
+                .fill(Color.primary)
+                .opacity(0.2)
+                .frame(width: circleDiameter, height: circleDiameter)
+            ForEach(0..<60) { i in
+                Rectangle()
+                    .opacity(0.7)
+                    .frame(width: 2, height: (i % 5 == 0) ? 20 : 10)
+                    .offset(x: 0, y: -circleDiameter/2.1)
+                    .rotationEffect(Angle(degrees: Double(i * 6)))
+            }
+            Rectangle()
+                .frame(width: 3, height: circleDiameter/2)
+                .offset(x: 0, y: -circleDiameter/4)
+                .rotationEffect(Angle(degrees: Double(second * 6)))
+        }
+        .onReceive(receiver, perform: { _ in
+            withAnimation(Animation.linear(duration: 0.01)) {
+                second += 1
+            }
+        })
     }
 }
 
