e78798b3b42375d2fcd6b8a61d3206c6f9ac6fa5
simfony <49770335+wizard1238@users.noreply.github.com>
2021-07-17 13:13:39 -0700

    Initial Commit

---
+++ 7-17-pomodoro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  7-17-pomodoro
+//
+//  Created by Jeremy Tow on 7/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 7-17-pomodoro/__17_pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  __17_pomodoroApp.swift
+//  7-17-pomodoro
+//
+//  Created by Jeremy Tow on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct __17_pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
